b76c82e80d545bd39b7d7aa8bdbfbea80d175d1f
anilrajpurohit <anilraj.gsa11@gmail.com>
2024-06-22 19:09:24 +0530

    updated

--- src/components/layout/Sidenav.js
+++ src/components/layout/Sidenav.js
@@ -1,16 +1,3 @@
-/*!
-  =========================================================
-  * Muse Ant Design Dashboard - v1.0.0
-  =========================================================
-  * Product Page: https://www.creative-tim.com/product/muse-ant-design-dashboard
-  * Copyright 2021 Creative Tim (https://www.creative-tim.com)
-  * Licensed under MIT (https://github.com/creativetimofficial/muse-ant-design-dashboard/blob/main/LICENSE.md)
-  * Coded by Creative Tim
-  =========================================================
-  * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
-
-// import { useState } from "react";
 import { Menu, Button } from "antd";
 import { NavLink, useLocation } from "react-router-dom";
 import logo from "../../assets/images/logo.png";
@@ -41,6 +28,138 @@ function Sidenav({ color }) {
       ></path>
     </svg>,
   ];
+
+const addPayment = [
+    <svg
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      key={0}
+    >
+      <path
+        d="M9 2C8.44772 2 8 2.44772 8 3C8 3.55228 8.44772 4 9 4H11C11.5523 4 12 3.55228 12 3C12 2.44772 11.5523 2 11 2H9Z"
+        fill={color}
+      ></path>
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M4 5C4 3.89543 4.89543 3 6 3C6 4.65685 7.34315 6 9 6H11C12.6569 6 14 4.65685 14 3C15.1046 3 16 3.89543 16 5V16C16 17.1046 15.1046 18 14 18H6C4.89543 18 4 17.1046 4 16V5ZM7 9C6.44772 9 6 9.44772 6 10C6 10.5523 6.44772 11 7 11H7.01C7.56228 11 8.01 10.5523 8.01 10C8.01 9.44772 7.56228 9 7.01 9H7ZM10 9C9.44772 9 9 9.44772 9 10C9 10.5523 9.44772 11 10 11H13C13.5523 11 14 10.5523 14 10C14 9.44772 13.5523 9 13 9H10ZM7 13C6.44772 13 6 13.4477 6 14C6 14.5523 6.44772 15 7 15H7.01C7.56228 15 8.01 14.5523 8.01 14C8.01 13.4477 7.56228 13 7.01 13H7ZM10 13C9.44772 13 9 13.4477 9 14C9 14.5523 9.44772 15 10 15H13C13.5523 15 14 14.5523 14 14C14 13.4477 13.5523 13 13 13H10Z"
+        fill={color}
+      ></path>
+    </svg>,
+  ];
+
+  const userDetails = [
+    <svg
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      key={0}
+    >
+      <path
+        d="M9 2C8.44772 2 8 2.44772 8 3C8 3.55228 8.44772 4 9 4H11C11.5523 4 12 3.55228 12 3C12 2.44772 11.5523 2 11 2H9Z"
+        fill={color}
+      ></path>
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M4 5C4 3.89543 4.89543 3 6 3C6 4.65685 7.34315 6 9 6H11C12.6569 6 14 4.65685 14 3C15.1046 3 16 3.89543 16 5V16C16 17.1046 15.1046 18 14 18H6C4.89543 18 4 17.1046 4 16V5ZM7 9C6.44772 9 6 9.44772 6 10C6 10.5523 6.44772 11 7 11H7.01C7.56228 11 8.01 10.5523 8.01 10C8.01 9.44772 7.56228 9 7.01 9H7ZM10 9C9.44772 9 9 9.44772 9 10C9 10.5523 9.44772 11 10 11H13C13.5523 11 14 10.5523 14 10C14 9.44772 13.5523 9 13 9H10ZM7 13C6.44772 13 6 13.4477 6 14C6 14.5523 6.44772 15 7 15H7.01C7.56228 15 8.01 14.5523 8.01 14C8.01 13.4477 7.56228 13 7.01 13H7ZM10 13C9.44772 13 9 13.4477 9 14C9 14.5523 9.44772 15 10 15H13C13.5523 15 14 14.5523 14 14C14 13.4477 13.5523 13 13 13H10Z"
+        fill={color}
+      ></path>
+    </svg>,
+  ];
+
+ const paymentHistory = [
+    <svg
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      key={0}
+    >
+      <path
+        d="M9 2C8.44772 2 8 2.44772 8 3C8 3.55228 8.44772 4 9 4H11C11.5523 4 12 3.55228 12 3C12 2.44772 11.5523 2 11 2H9Z"
+        fill={color}
+      ></path>
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M4 5C4 3.89543 4.89543 3 6 3C6 4.65685 7.34315 6 9 6H11C12.6569 6 14 4.65685 14 3C15.1046 3 16 3.89543 16 5V16C16 17.1046 15.1046 18 14 18H6C4.89543 18 4 17.1046 4 16V5ZM7 9C6.44772 9 6 9.44772 6 10C6 10.5523 6.44772 11 7 11H7.01C7.56228 11 8.01 10.5523 8.01 10C8.01 9.44772 7.56228 9 7.01 9H7ZM10 9C9.44772 9 9 9.44772 9 10C9 10.5523 9.44772 11 10 11H13C13.5523 11 14 10.5523 14 10C14 9.44772 13.5523 9 13 9H10ZM7 13C6.44772 13 6 13.4477 6 14C6 14.5523 6.44772 15 7 15H7.01C7.56228 15 8.01 14.5523 8.01 14C8.01 13.4477 7.56228 13 7.01 13H7ZM10 13C9.44772 13 9 13.4477 9 14C9 14.5523 9.44772 15 10 15H13C13.5523 15 14 14.5523 14 14C14 13.4477 13.5523 13 13 13H10Z"
+        fill={color}
+      ></path>
+    </svg>,
+  ];
+
+ const role = [
+    <svg
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      key={0}
+    >
+      <path
+        d="M9 2C8.44772 2 8 2.44772 8 3C8 3.55228 8.44772 4 9 4H11C11.5523 4 12 3.55228 12 3C12 2.44772 11.5523 2 11 2H9Z"
+        fill={color}
+      ></path>
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M4 5C4 3.89543 4.89543 3 6 3C6 4.65685 7.34315 6 9 6H11C12.6569 6 14 4.65685 14 3C15.1046 3 16 3.89543 16 5V16C16 17.1046 15.1046 18 14 18H6C4.89543 18 4 17.1046 4 16V5ZM7 9C6.44772 9 6 9.44772 6 10C6 10.5523 6.44772 11 7 11H7.01C7.56228 11 8.01 10.5523 8.01 10C8.01 9.44772 7.56228 9 7.01 9H7ZM10 9C9.44772 9 9 9.44772 9 10C9 10.5523 9.44772 11 10 11H13C13.5523 11 14 10.5523 14 10C14 9.44772 13.5523 9 13 9H10ZM7 13C6.44772 13 6 13.4477 6 14C6 14.5523 6.44772 15 7 15H7.01C7.56228 15 8.01 14.5523 8.01 14C8.01 13.4477 7.56228 13 7.01 13H7ZM10 13C9.44772 13 9 13.4477 9 14C9 14.5523 9.44772 15 10 15H13C13.5523 15 14 14.5523 14 14C14 13.4477 13.5523 13 13 13H10Z"
+        fill={color}
+      ></path>
+    </svg>,
+  ];
+
+   const staff = [
+      <svg
+        width="20"
+        height="20"
+        viewBox="0 0 20 20"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        key={0}
+      >
+        <path
+          d="M9 2C8.44772 2 8 2.44772 8 3C8 3.55228 8.44772 4 9 4H11C11.5523 4 12 3.55228 12 3C12 2.44772 11.5523 2 11 2H9Z"
+          fill={color}
+        ></path>
+        <path
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M4 5C4 3.89543 4.89543 3 6 3C6 4.65685 7.34315 6 9 6H11C12.6569 6 14 4.65685 14 3C15.1046 3 16 3.89543 16 5V16C16 17.1046 15.1046 18 14 18H6C4.89543 18 4 17.1046 4 16V5ZM7 9C6.44772 9 6 9.44772 6 10C6 10.5523 6.44772 11 7 11H7.01C7.56228 11 8.01 10.5523 8.01 10C8.01 9.44772 7.56228 9 7.01 9H7ZM10 9C9.44772 9 9 9.44772 9 10C9 10.5523 9.44772 11 10 11H13C13.5523 11 14 10.5523 14 10C14 9.44772 13.5523 9 13 9H10ZM7 13C6.44772 13 6 13.4477 6 14C6 14.5523 6.44772 15 7 15H7.01C7.56228 15 8.01 14.5523 8.01 14C8.01 13.4477 7.56228 13 7.01 13H7ZM10 13C9.44772 13 9 13.4477 9 14C9 14.5523 9.44772 15 10 15H13C13.5523 15 14 14.5523 14 14C14 13.4477 13.5523 13 13 13H10Z"
+          fill={color}
+        ></path>
+      </svg>,
+    ];
+
+  const tracking = [
+      <svg
+        width="20"
+        height="20"
+        viewBox="0 0 20 20"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        key={0}
+      >
+        <path
+          d="M9 2C8.44772 2 8 2.44772 8 3C8 3.55228 8.44772 4 9 4H11C11.5523 4 12 3.55228 12 3C12 2.44772 11.5523 2 11 2H9Z"
+          fill={color}
+        ></path>
+        <path
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M4 5C4 3.89543 4.89543 3 6 3C6 4.65685 7.34315 6 9 6H11C12.6569 6 14 4.65685 14 3C15.1046 3 16 3.89543 16 5V16C16 17.1046 15.1046 18 14 18H6C4.89543 18 4 17.1046 4 16V5ZM7 9C6.44772 9 6 9.44772 6 10C6 10.5523 6.44772 11 7 11H7.01C7.56228 11 8.01 10.5523 8.01 10C8.01 9.44772 7.56228 9 7.01 9H7ZM10 9C9.44772 9 9 9.44772 9 10C9 10.5523 9.44772 11 10 11H13C13.5523 11 14 10.5523 14 10C14 9.44772 13.5523 9 13 9H10ZM7 13C6.44772 13 6 13.4477 6 14C6 14.5523 6.44772 15 7 15H7.01C7.56228 15 8.01 14.5523 8.01 14C8.01 13.4477 7.56228 13 7.01 13H7ZM10 13C9.44772 13 9 13.4477 9 14C9 14.5523 9.44772 15 10 15H13C13.5523 15 14 14.5523 14 14C14 13.4477 13.5523 13 13 13H10Z"
+          fill={color}
+        ></path>
+      </svg>,
+    ];
 
   const tables = [
     <svg
@@ -160,112 +279,206 @@ function Sidenav({ color }) {
     </svg>,
   ];
 
-  return (
+  return(
     <>
       <div className="brand">
         <img src={logo} alt="" />
         <span>Muse Dashboard</span>
       </div>
       <hr />
-      <Menu theme="light" mode="inline">
-        <Menu.Item key="1">
-          <NavLink to="/dashboard">
-            <span
-              className="icon"
-              style={{
-                background: page === "dashboard" ? color : "",
-              }}
-            >
-              {dashboard}
-            </span>
-            <span className="label">Dashboard</span>
-          </NavLink>
-        </Menu.Item>
-        <Menu.Item key="2">
-          <NavLink to="/tables">
-            <span
-              className="icon"
-              style={{
-                background: page === "tables" ? color : "",
-              }}
-            >
-              {tables}
-            </span>
-            <span className="label">Tables</span>
-          </NavLink>
-        </Menu.Item>
-        <Menu.Item key="3">
-          <NavLink to="/billing">
-            <span
-              className="icon"
-              style={{
-                background: page === "billing" ? color : "",
-              }}
-            >
-              {billing}
-            </span>
-            <span className="label">Billing</span>
-          </NavLink>
-        </Menu.Item>
-        <Menu.Item key="4">
-          <NavLink to="/rtl">
-            <span
-              className="icon"
-              style={{
-                background: page === "rtl" ? color : "",
-              }}
-            >
-              {rtl}
-            </span>
-            <span className="label">RTL</span>
-          </NavLink>
-        </Menu.Item>
-        <Menu.Item className="menu-item-header" key="5">
-          Account Pages
-        </Menu.Item>
-        <Menu.Item key="6">
-          <NavLink to="/profile">
-            <span
-              className="icon"
-              style={{
-                background: page === "profile" ? color : "",
-              }}
-            >
-              {profile}
-            </span>
-            <span className="label">Profile</span>
-          </NavLink>
-        </Menu.Item>
-        <Menu.Item key="7">
-          <NavLink to="/sign-in">
-            <span className="icon">{signin}</span>
-            <span className="label">Sign In</span>
-          </NavLink>
-        </Menu.Item>
-        <Menu.Item key="8">
-          <NavLink to="/sign-up">
-            <span className="icon">{signup}</span>
-            <span className="label">Sign Up</span>
-          </NavLink>
-        </Menu.Item>
+          <Menu theme="light" mode="inline">
+            <Menu.Item key="1">
+              <NavLink to="/dashboard">
+                <span
+                  className="icon"
+                  style={{
+                    background: page === "dashboard" ? color : "",
+                  }}
+                >
+                  {dashboard}
+                </span>
+                <span className="label">Dashboard</span>
+              </NavLink>
+            </Menu.Item>
+
+            <Menu.Item key="2">
+                  <NavLink to="/addPayment">
+                        <span
+                          className="icon"
+                          style={{
+                            background: page === "addPayment" ? color : "",
+                          }}
+                        >
+                          {addPayment}
+                        </span>
+                        <span className="label">Add Payment</span>
+                  </NavLink>
+            </Menu.Item>
+
+            <Menu.Item key="3">
+                  <NavLink to="/userDetails">
+                        <span
+                          className="icon"
+                          style={{
+                            background: page === "userDetails" ? color : "",
+                          }}
+                        >
+                          {userDetails}
+                        </span>
+                        <span className="label">User Details</span>
+                  </NavLink>
+            </Menu.Item>
+
+            <Menu.Item key="4">
+              <NavLink to="/paymentHisotry">
+                    <span
+                      className="icon"
+                      style={{
+                        background: page === "paymentHistory" ? color : "",
+                      }}
+                    >
+                      {paymentHistory}
+                    </span>
+                    <span className="label">Payment History</span>
+              </NavLink>
+            </Menu.Item>
+
+            <Menu.Item key="5">
+              <NavLink to="/role">
+                    <span
+                      className="icon"
+                      style={{
+                        background: page === "role" ? color : "",
+                      }}
+                    >
+                      {role}
+                    </span>
+                    <span className="">Role</span>
+              </NavLink>
+            </Menu.Item>
+
+            <Menu.Item key="6">
+                <NavLink to="/staff">
+                        <span
+                          className="icon"
+                          style={{
+                            background: page === "staff" ? color : "",
+                          }}
+                        >
+                          {staff}
+                        </span>
+                        <span className="">Staff</span>
+                </NavLink>
+            </Menu.Item>
+
+            <Menu.Item key="7">
+              <NavLink to="/tracking">
+                    <span
+                      className="icon"
+                      style={{
+                        background: page === "tracking" ? color : "",
+                      }}
+                    >
+                      {tracking}
+                    </span>
+                    <span className="label">Tracking</span>
+              </NavLink>
+            </Menu.Item>
+
+
+            <Menu.Item key="8">
+              <NavLink to="/tables">
+                    <span
+                      className="icon"
+                      style={{
+                        background: page === "tables" ? color : "",
+                      }}
+                    >
+                      {tables}
+                    </span>
+                    <span className="label">Tables</span>
+              </NavLink>
+            </Menu.Item>
+
+            <Menu.Item key="9">
+              <NavLink to="/billing">
+                    <span
+                      className="icon"
+                      style={{
+                        background: page === "billing" ? color : "",
+                      }}
+                    >
+                      {billing}
+                    </span>
+                    <span className="label">Billing</span>
+              </NavLink>
+            </Menu.Item>
+
+            <Menu.Item key="10">
+              <NavLink to="/rtl">
+                <span
+                  className="icon"
+                  style={{
+                    background: page === "rtl" ? color : "",
+                  }}
+                >
+                  {rtl}
+                </span>
+                <span className="label">RTL</span>
+              </NavLink>
+            </Menu.Item>
+
+            <Menu.Item className="menu-item-header" key="11">
+              Account Pages
+            </Menu.Item>
+
+            <Menu.Item key="12">
+              <NavLink to="/profile">
+                <span
+                  className="icon"
+                  style={{
+                    background: page === "profile" ? color : "",
+                  }}
+                >
+                  {profile}
+                </span>
+                <span className="label">Profile</span>
+              </NavLink>
+            </Menu.Item>
+
+            <Menu.Item key="13">
+              <NavLink to="/sign-in">
+                <span className="icon">{signin}</span>
+                <span className="label">Sign In</span>
+              </NavLink>
+            </Menu.Item>
+
+            <Menu.Item key="14">
+              <NavLink to="/sign-up">
+                <span className="icon">{signup}</span>
+                <span className="label">Sign Up</span>
+              </NavLink>
+            </Menu.Item>
+
       </Menu>
+
+
       <div className="aside-footer">
-        <div
-          className="footer-box"
-          style={{
-            background: color,
-          }}
-        >
-          <span className="icon" style={{ color }}>
-            {dashboard}
-          </span>
-          <h6>Need Help?</h6>
-          <p>Please check our docs</p>
-          <Button type="primary" className="ant-btn-sm ant-btn-block">
-            DOCUMENTATION
-          </Button>
-        </div>
+            <div className="footer-box" style={{background: color,}}>
+                  <span className="icon" style={{ color }}>
+                    {dashboard}
+                  </span>
+              <h6>Need Help?</h6>
+
+              <p>Please check our docs</p>
+
+              <Button type="primary" className="ant-btn-sm ant-btn-block">
+                DOCUMENTATION
+              </Button>
+
+            </div>
       </div>
+
     </>
   );
 }
